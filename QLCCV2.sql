3e87afedbdf3c1160ead28d963a7587c453b7732
1660701 <44780140+1660701@users.noreply.github.com>
2019-06-13 22:07:09 +0700

    capnhat

--- QLCCV2.sql
+++ QLCCV2.sql
@@ -1,77 +1,7 @@
-USE [master]
+ÔªøUSE [master]
 GO
 /****** Object:  Database [QuanLyCungCapVideo]    Script Date: 05/06/2019 5:36:19 CH ******/
 CREATE DATABASE [QuanLyCungCapVideo]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'QuanLyCungCapVideo', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.SQL2014\MSSQL\DATA\QuanLyCungCapVideo.mdf' , SIZE = 3264KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
- LOG ON 
-( NAME = N'QuanLyCungCapVideo_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.SQL2014\MSSQL\DATA\QuanLyCungCapVideo_log.ldf' , SIZE = 832KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET COMPATIBILITY_LEVEL = 120
-GO
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [QuanLyCungCapVideo].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET ANSI_NULL_DEFAULT OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET ANSI_NULLS OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET ANSI_PADDING OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET ANSI_WARNINGS OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET ARITHABORT OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET AUTO_CLOSE ON 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET AUTO_SHRINK OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET AUTO_UPDATE_STATISTICS ON 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET CURSOR_DEFAULT  GLOBAL 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET NUMERIC_ROUNDABORT OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET QUOTED_IDENTIFIER OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET RECURSIVE_TRIGGERS OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET  ENABLE_BROKER 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET TRUSTWORTHY OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET PARAMETERIZATION SIMPLE 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET HONOR_BROKER_PRIORITY OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET RECOVERY SIMPLE 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET  MULTI_USER 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET PAGE_VERIFY CHECKSUM  
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET DB_CHAINING OFF 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET TARGET_RECOVERY_TIME = 0 SECONDS 
-GO
-ALTER DATABASE [QuanLyCungCapVideo] SET DELAYED_DURABILITY = DISABLED 
 GO
 USE [QuanLyCungCapVideo]
 GO
@@ -123,7 +53,7 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[TAIKHOAN](
 	[IDTK] [int] IDENTITY(1,1) NOT NULL,
-	[TenND] [nvarchar](50) NULL,
+	[TenND] [nvarchar](100) NULL,
 	[MatKhau] [varchar](50) NULL,
 	[DienThoai] [varchar](50) NULL,
 	[Email] [varchar](50) NULL,
@@ -164,7 +94,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[THELOAI](
 	[IDTL] [int] IDENTITY(1,1) NOT NULL,
-	[TenTL] [nvarchar](50) NULL,
+	[TenTL] [nvarchar](200) NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[IDTL] ASC
@@ -181,14 +111,14 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[VIDEO](
 	[IDVideo] [int] IDENTITY(1,1) NOT NULL,
-	[TenVideo] [nvarchar](50) NULL,
+	[TenVideo] [nvarchar](100) NULL,
 	[IDTL] [int] NULL,
 	[QuocGia] [nvarchar](50) NULL,
 	[ThoiLuong] [varchar](30) NULL,
 	[DaoDien] [nvarchar](50) NULL,
 	[LuotXem] [int] NULL,
-	[LinkVideo] [varchar](50) NOT NULL,
-	[LinkPoster] [varchar](50) NULL,
+	[LinkVideo] [nvarchar](200) NOT NULL,
+	[LinkPoster] [nvarchar](200) NULL,
  CONSTRAINT [VIDEO_pk] PRIMARY KEY CLUSTERED 
 (
 	[IDVideo] ASC,
@@ -209,8 +139,8 @@ GO
 CREATE TABLE [dbo].[YEUTHICH](
 	[IDYT] [int] IDENTITY(1,1) NOT NULL,
 	[IDTK] [int] NULL,
-	[LinkVideo] [varchar](50) NULL,
-	[LinkPoster] [varchar](50) NULL,
+	[LinkVideo] [nvarchar](200) NULL,
+	[LinkPoster] [nvarchar](200) NULL,
 	[IDVideo] [int] NULL,
 PRIMARY KEY CLUSTERED 
 (
@@ -233,37 +163,15 @@ INSERT [dbo].[TAIKHOAN] ([IDTK], [TenND], [MatKhau], [DienThoai], [Email], [IDQu
 SET IDENTITY_INSERT [dbo].[TAIKHOAN] OFF
 SET IDENTITY_INSERT [dbo].[THELOAI] ON 
 
-INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (1, N'TÏnh C?m')
-INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (2, N'Vi?n T??ng')
-INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (3, N'H‡nh ??ng')
-INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (4, N'PhiÍu L?u')
-INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (5, N'Ho?t HÏnh')
-INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (6, N'H‡i K?ch')
-INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (7, N'¬m Nh?c')
+INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (1, N'T√¨nh C·∫£m')
+INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (2, N'Vi·ªÖn T∆∞·ªüng')
+INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (3, N'H√†nh ƒê·ªông')
+INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (4, N'Phi√™u L∆∞u')
+INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (5, N'Ho·∫°t H√¨nh')
+INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (6, N'H√†i K·ªãch')
+INSERT [dbo].[THELOAI] ([IDTL], [TenTL]) VALUES (7, N'√Çm Nh·∫°c')
 SET IDENTITY_INSERT [dbo].[THELOAI] OFF
-SET IDENTITY_INSERT [dbo].[VIDEO] ON 
 
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (1, N'N?m B??c ?? YÍu', 1, N'M?', N'116 p', N'Justin Baldoni', 23, N'nambuocdeyeu.mp4', N'1.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (2, N'Ch‡ng Ng? T?p YÍu', 1, N'Romania', N'109 p', N'Cristina Jacob', 21, N'changngotapyeu.mp4', N'2.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (3, N'LuÙn LuÙn CÛ Th?', 1, N'M?', N'101 p', N'Nahnatchka Khan', 22, N'luonluoncothe.mp4', N'3.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (4, N'MÙi HÙn Ng?t Ng‡o', 1, N'Anh', N'108 p', N'Annabel Jankel', 35, N'moihocngotngao.mp4', N'4.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (5, N'M?i L?', 1, N'M?', N'117 p', N'Drake Doremus', 15, N'moila.mp4', N'5.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (6, N'Mong Em H?nh Ph˙c ', 1, N'?‡i Loan', N'105 p', N'Gavin Lin', 24, N'mongemhanhphuc.mp4', N'6.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (7, N'Ng??i B?n Tuy?t V?i', 1, N'M?', N'92 p', N'Jennifer Kaytin Robinson', 15, N'nguoibantuyetvoi.mp4', N'7.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (8, N'H? Cu?i', 1, N'M?', N'109 p', N'William Bindley', 43, N'hacuoi.mp4', N'8.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (9, N'Bi?t ??i SiÍu Anh H˘ng 4: T‡n Cu?c', 2, N'M?', N'181 p', N'Joe Russo, Anthony Russo', 45, N'endgame.mp4', N'9.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (10, N'Qu? ??', 2, N'M?, Anh, Bulgaria', N'120 p', N'Neil Marshall', 23, N'quydo.mp4', N'10.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (11, N'T? ThiÍn ??i Th·nh: H?a Di?n S?n', 2, N'Trung Qu?c', N'90 p', N'Zhenzhao Lin', 22, N'tethiendaithanh.mp4', N'11.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (12, N'Khu Ngh? D??ng X·c S?ng', 2, N'Anh, T‚y Ban Nha, Belgium', N'93 p', N'Steve Barker,', 33, N'khunghiduongsacsong.mp4', N'12.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (13, N'?? Ch? M?i', 2, N'M?', N'109 p', N'Rupert Wyatt', 12, N'dechemoi.mp4', N'13.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (14, N'B? Ba Qu·i Nh‚n', 2, N'M?', N'128 p', N'M. Night Shyamalan,', 43, N'bobaquainhan.mp4', N'14.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (15, N'??i ⁄y MARVEL', 2, N'M?, ⁄c', N'123 P', N'Anna Boden, Ryan Fleck, ', 24, N'daiuy.mp4', N'15.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (16, N'Gi?i C?u Th? Gi?i', 2, N'M?', N'98 p', N'M?,', 34, N'giaicuuthegioi.mp4', N'16.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (17, N'T˘ Nh‚n B·o Th˘', 3, N'Anh', N'90 P', N'Jesse V. Johnson,', 12, N'tunhanbaothu.mp4', N'17.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (18, N'K? Ph· H?y', 3, N'M?', N'121 p', N'Karyn Kusama,', 43, N'kephahuy.mp4', N'18.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (19, N'BÙng H?ng S·t Th?', 3, N'Philippines', N'90 p', N'Pedring Lopez', 22, N'bonghongsatthu.mp4', N'19.jpg')
-INSERT [dbo].[VIDEO] ([IDVideo], [TenVideo], [IDTL], [QuocGia], [ThoiLuong], [DaoDien], [LuotXem], [LinkVideo], [LinkPoster]) VALUES (20, N'B?u Tr?i ThÈp', 3, N'⁄c, ??c, Finland', N'93 p', N'Timo Vuorensola,', 32, N'bautroithep.mp4', N'20.jpg')
-SET IDENTITY_INSERT [dbo].[VIDEO] OFF
 ALTER TABLE [dbo].[TAIKHOAN]  WITH CHECK ADD  CONSTRAINT [FK_TK_Q] FOREIGN KEY([IDQuyen])
 REFERENCES [dbo].[QUYEN] ([IDQuyen])
 GO
---
+++ QuanLyCungCapVideo (capnhat)/QuanLyCungCapVideo/UserControl1/VideoAppearance.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using QuanLyCungCapVideo.Model;
+using QuanLyCungCapVideo.ViewModel;
+
+namespace QuanLyCungCapVideo.UserControl1
+{
+    /// <summary>
+    /// Interaction logic for VideoAppearance.xaml
+    /// </summary>
+    public partial class VideoAppearance : UserControl
+    {
+
+        private ObservableCollection<VIDEO> list;
+        public ObservableCollection<VIDEO> List { get => list; set { list = value; DataContext = list; } }
+        //VideoAppearanceViewModel vmd = new VideoAppearanceViewModel();
+
+        public VideoAppearance()
+        {
+            InitializeComponent();
+            list = new ObservableCollection<VIDEO>();
+            DataContext = list;
+            //DataContext = vmd.List;
+        }       
+        
+
+    }
+}
