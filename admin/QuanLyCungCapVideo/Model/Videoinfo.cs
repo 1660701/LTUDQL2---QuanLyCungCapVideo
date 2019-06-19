79e25320253fb03bd24a1671d08c4bc02ae83c73
Nguyễn Anh Tuấn <45095135+1660689@users.noreply.github.com>
2019-06-19 18:55:37 +0700

    Admin

---
+++ admin/QuanLyCungCapVideo/Model/Videoinfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyCungCapVideo.Model
+{
+    public class Videoinfo : DependencyObject 
+    {
+        public static readonly DependencyProperty TitleProperty;
+        public static readonly DependencyProperty PathProperty;
+        public static readonly DependencyProperty DecriptionProperty;
+        static Videoinfo()
+        {
+            TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Videoinfo), new PropertyMetadata("New Video"));
+            TitleProperty = DependencyProperty.Register("Path", typeof(string), typeof(Videoinfo), new PropertyMetadata(@"H:\zzzVideo\nes.mp4"));
+            TitleProperty = DependencyProperty.Register("Decription", typeof(string), typeof(Videoinfo), new PropertyMetadata(@"New secription"));
+        }
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public string Path
+        {
+            get { return (string)GetValue(PathProperty); }
+            set { SetValue(PathProperty, value); }
+        }
+        public string Decription
+        {
+            get { return (string)GetValue(DecriptionProperty); }
+            set { SetValue(DecriptionProperty, value); }
+        }
+    }
+}
---
+++ admin/QuanLyCungCapVideo/WdlistVideo.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using QuanLyCungCapVideo.Model;
+
+namespace QuanLyCungCapVideo
+{
+    /// <summary>
+    /// Interaction logic for WdlistVideo.xaml
+    /// </summary>
+    public partial class WdlistVideo : Window
+    {
+        public WdlistVideo()
+        {
+            InitializeComponent();
+            DataContext = new List<Videoinfo>
+            {
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{},
+                new Videoinfo{}
+            };
+        }
+    }
+}
